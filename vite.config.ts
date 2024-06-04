ea661fc939465c5582312838cf028eb14c9b7216
Patryk Markowski <patrol56789@gmail.com>
2024-06-04 21:12:25 +0200

    deploy update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import adapter from '@sveltejs/adapter-static';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/tiled3d/'
 })
